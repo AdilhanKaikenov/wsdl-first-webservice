e3889dbb2e393e4414c40969d06e79029cba831d
Kaikenov Adilhan <adilhan_kai@mail.ru>
2020-12-31 23:37:01 +0600

    Initial commit

---
+++ src/main/java/com/kaikenov/ws/soap/WsdlFirstWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kaikenov.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlFirstWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdlFirstWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaikenov/ws/soap/WsdlFirstWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikenov.ws.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlFirstWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
