bb7fecbd9b3ecd0e1f8db64b7e3d6ad00edc2736
Kaikenov Adilhan <adilhan_kai@mail.ru>
2021-01-01 16:03:57 +0600

    soap handler

--- src/main/java/com/kaikenov/ws/soap/configuration/WebServiceConfiguration.java
+++ src/main/java/com/kaikenov/ws/soap/configuration/WebServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.kaikenov.ws.soap.configuration;
 
 import com.kaikenov.ws.soap.CustomerOrderWsImpl;
+import com.kaikenov.ws.soap.handler.SiteHandler;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.soap.SOAPBinding;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class WebServiceConfiguration {
@@ -19,6 +24,11 @@ public class WebServiceConfiguration {
     public Endpoint endpoint() {
         Endpoint endpoint = new EndpointImpl(bus, new CustomerOrderWsImpl());
         endpoint.publish("/customer-order-service");
+
+        SOAPBinding binding = (SOAPBinding) endpoint.getBinding();
+        List<Handler> handlerChain = new ArrayList<>();
+        handlerChain.add(new SiteHandler());
+        binding.setHandlerChain(handlerChain);
 
         return endpoint;
     }
